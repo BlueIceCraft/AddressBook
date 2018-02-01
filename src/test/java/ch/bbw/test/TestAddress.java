ef87a63698683dee0c2d4995a9c388fbf507ea1b
5ia15matomaszewski <5ia15matomaszewski@gibwintra.ch>
2018-02-01 13:37:30 +0100

    removed mac only files and added tests

--- src/test/java/ch/bbw/test/TestAddress.java
+++ src/test/java/ch/bbw/test/TestAddress.java
@@ -20,22 +20,24 @@ public class TestAddress {
 	
 	@Test
 	public void testConstructor1() {
-		Address address = new Address(1, "Peter", "Muster", "000 0000 0000");
+		Address address = new Address(1, "Peter", "Muster", "000 0000 0000", "TestDorf");
 		assertEquals(address.getId(), 1);
 		assertTrue(address.getFirstname().equals("Peter"));
 		assertTrue(address.getLastname().equals("Muster"));
 		assertTrue(address.getPhonenumber().equals("000 0000 0000"));
+		assertTrue(address.getVillage().equals("TestDorf"));
 		assertNull(address.getRegistrationDate());
 	}
 	
 	@Test
 	public void testConstructor2() {
 		Date date = new Date();
-		Address address = new Address(1, "Peter", "Muster", "000 0000 0000", date);
+		Address address = new Address(1, "Peter", "Muster", "000 0000 0000", "TestDorf", date);
 		assertEquals(address.getId(), 1);
 		assertTrue(address.getFirstname().equals("Peter"));
 		assertTrue(address.getLastname().equals("Muster"));
 		assertTrue(address.getPhonenumber().equals("000 0000 0000"));
+		assertTrue(address.getVillage().equals("TestDorf"));
 		assertNotNull(address.getRegistrationDate());
 		assertTrue(address.getRegistrationDate().equals(date));
 	}
